03f62f20eff995ffa19cf32b98ae640d0ce3ad6a
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2018-06-21 16:07:00 -0500

    Calculates ETA from forge to selected hospital

--- src/components/Eta.js
+++ src/components/Eta.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View } from 'react-native';
+import { Picker, Icon } from 'native-base';
+import moment from 'moment';
 
 var googleMapsClient = require('react-native-google-maps-services').createClient({
   key: 'API KEY'
@@ -8,13 +10,16 @@ var googleMapsClient = require('react-native-google-maps-services').createClient
 
 export default class Eta extends Component {
   state = {
-    someText: 'abc',
-    currentPosition: null
+    someText: '',
+    currentPosition: null,
+    destination: '1111 6th ave, des moines, ia 50314'
   }
 
   showDuration = (err, response) => {
     if(!err) {
-      this.setState({someText: response.json.routes[0].legs[0].duration.text});
+      var seconds = response.json.routes[0].legs[0].duration.value;
+      var arrivalTime = moment().add(seconds, 's');
+      this.setState({someText: arrivalTime.format("h:mm a")});
     }
   }
 
@@ -24,25 +29,32 @@ export default class Eta extends Component {
 
   getDirections = () => {
     currentLocation = this.getGeoLocation();
-    console.log(currentLocation);
     googleMapsClient.directions({
       origin: {
         lat: this.state.currentPosition.coords.latitude,
         lng: this.state.currentPosition.coords.longitude
       },
-      destination: '1111 6th ave, des moines, ia 50314',
+      destination: this.state.destination,
       mode: 'driving'
     }, this.showDuration);
   }
   render() {
-    const location = this.state.currentPosition ?
-      `${this.state.currentPosition.coords.latitude}, ${this.state.currentPosition.coords.longitude}` :
-      'Unknown';
-
     return <View style={styles.eta}>
-      <Text>{location}</Text>
-      <Text>{this.state.someText}</Text>
-      <Button onPress={this.getDirections} title="Get ETA"/>
+      <Picker
+        mode='dropdown'
+        iosIcon={<Icon name="arrow-dropdown-circle"
+                style={{ color: "#007aff", fontSize: 25 }} />}
+        iosHeader='Choose destination'
+        selectedValue={this.state.destination}
+        style={{ height: 50, width: undefined }}
+        onValueChange={(itemValue, itemIndex) => this.setState({destination: itemValue, someText: ''})}>
+        <Picker.Item label='Mercy' value='1111 6th ave, des moines, ia 50314' />
+        <Picker.Item label='Methodist' value='1200 Pleasant St, Des Moines, IA 50309' />
+        <Picker.Item label='Broadlawn' value='1801 Hickman Rd, Des Moines, IA 50314' />
+        <Picker.Item label='Lutheran' value='700 E University Ave, Des Moines, IA 50316' />
+      </Picker>
+      <Button disabled={this.state.currentPosition===null} onPress={this.getDirections} title="Get ETA from Forge"/>
+      <Text style={{margin: 10}}>{this.state.someText}</Text>
     </View>
   }
 
